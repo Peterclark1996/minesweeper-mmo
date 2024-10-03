e9def6c30167b42421036b57fe046d4c18d7742f
Pete <peterclark1996@gmail.com>
2024-10-03 22:07:57 +0100

    Setup nodemon

---
+++ src/server/start-server.ts
@@ -0,0 +1,56 @@
+import * as http from "http"
+import { Server as SocketIOServer } from "socket.io"
+import { createServer as createViteServer } from "vite"
+import { CellState } from "../types/cell-state"
+import { ClientToServerEvents } from "../types/client-to-server-events"
+import { ServerToClientEvents } from "../types/server-to-client-events"
+
+export const startServer = async () => {
+    const vite = await createViteServer({
+        server: { middlewareMode: true }
+    })
+
+    const server = http.createServer()
+    server.on("request", vite.middlewares)
+
+    const io = new SocketIOServer<ClientToServerEvents, ServerToClientEvents>(server)
+
+    const gridSize = 20
+    const grid = {
+        rowCount: gridSize,
+        columnCount: gridSize,
+        cells: buildGrid(gridSize, gridSize)
+    }
+
+    io.on("connection", socket => {
+        console.log("A user connected")
+
+        socket.emit("gridUpdated", grid)
+
+        socket.on("clickCell", ({ row, column }) => {
+            console.log(`User clicked cell at row ${row} and column ${column}`)
+
+            io.emit("gridUpdated", grid)
+        })
+
+        socket.on("disconnect", () => {
+            console.log("A user disconnected")
+        })
+    })
+
+    server.listen(3000, () => {
+        console.log("Server is running at http://localhost:3000")
+    })
+}
+
+const buildGrid = (rowCount: number, columnCount: number) => {
+    const cells: CellState[][] = []
+    for (let row = 0; row < rowCount; row++) {
+        const row: CellState[] = []
+        for (let cell = 0; cell < columnCount; cell++) {
+            row.push({ type: "hidden", flagged: false })
+        }
+        cells.push(row)
+    }
+    return cells
+}
