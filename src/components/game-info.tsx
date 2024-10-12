3f59647b47af674e2fb6ba2239b793aba5d1e375
Pete <peterclark1996@gmail.com>
2024-10-12 17:00:35 +0100

    Add text to display finish state

--- src/components/game-info.tsx
+++ src/components/game-info.tsx
@@ -6,14 +6,26 @@ export const GameInfo = () => {
 
     const flags = server.gameState.cells.flat().filter(cell => cell.type === "hidden" && cell.flagged).length
 
-    return (
-        <div className="flex text-xs gap-12">
-            <span className="flex gap-4 ">
+    const getGameStateInfo = () => {
+        if (server.gameState.finishState === "lost") {
+            return <span className="text-three">Game Lost!</span>
+        }
+        if (server.gameState.finishState === "won") {
+            return <span className="text-two">Game Won!</span>
+        }
+        return (
+            <>
                 <p>Flagged Mines</p>
                 <p className="w-16">
                     {flags}/{server.gameState.mineCount}
                 </p>
-            </span>
+            </>
+        )
+    }
+
+    return (
+        <div className="flex text-xs gap-12">
+            <span className="flex gap-4 ">{getGameStateInfo()}</span>
             <span className="flex gap-4">
                 <p>Time left</p>
                 <Countdown />
